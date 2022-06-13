4ca0d569f358505ebc97116b0612fac9ff958e7d
carolchep <carolchepsam@gmail.com>
2022-06-13 15:39:41 +0300

    updating

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -12,6 +12,7 @@ export const getUser = async (req, res) => {
         if (user) {
             //if user exists
             //send everything excluding password
+            //destructuring user response
             const { password, ...otherDetails } = user._doc;
 
             res.status(200).json(otherDetails);
@@ -20,5 +21,29 @@ export const getUser = async (req, res) => {
         }
     } catch (error) {
         res.status(500).json(error);
+    }
+};
+// update a user
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId, currentUserAdminStatus, password } = req.body;
+
+    if (id === currentUserId || currentUserAdminStatus) {
+        try {
+            if (password) {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(password, salt);
+            }
+
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {
+                new: true,
+            });
+
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("you can only update your own profile");
     }
 };
