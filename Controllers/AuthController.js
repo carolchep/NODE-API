8a5e51ebaa2a5366ee3593e472e7463cc73d6e94
carolchep <carolchepsam@gmail.com>
2022-06-13 15:26:01 +0300

    login user controller

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -3,11 +3,13 @@ import bcrypt from "bcrypt";
 
 // Registering a new User
 export const registerUser = async (req, res) => {
+    //destrucure the request body
     const { username, password, firstname, lastname } = req.body;
-
+//use bcrypy to hash passsword
     const salt = await bcrypt.genSalt(10);
+    //attached hashed value to the passsword
     const hashedPass = await bcrypt.hash(password, salt);
-
+//create an object instance with the values
     const newUser = new UserModel({
         username,
         password: hashedPass,
@@ -16,9 +18,11 @@ export const registerUser = async (req, res) => {
     });
 
     try {
+        //save the object to the database
         await newUser.save();
         res.status(200).json(newUser);
     } catch (error) {
+        //error display
         res.status(500).json({ message: error.message });
     }
 };
@@ -27,18 +31,22 @@ export const registerUser = async (req, res) => {
 // login User
 
 export const loginUser = async (req, res) => {
+    //destructure the values
     const {username, password} = req.body
 
     try {
+        //find the username in the db
         const user = await UserModel.findOne({username: username})
 
 
         if(user)
+        //if user exists
         {
+            //use compare to check if paasword is the same
             const validity = await bcrypt.compare(password, user.password)
 
 
-            validity? res.status(200).json(user): res.status(400).json("Wrong Password")
+            validity? res.status(200).json(user): res.status(400).json("Invalid password")
         }
         else{
             res.status(404).json("User does not exists")
