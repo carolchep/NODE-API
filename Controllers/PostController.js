5fd035ac197e174191c3323f32981f5541171354
carolchep <carolchepsam@gmail.com>
2022-06-13 16:35:09 +0300

    deleting a post

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -25,3 +25,38 @@ export const getPost = async (req, res) => {
         res.status(500).json(error);
     }
 };
+
+// Update a post
+export const updatePost = async (req, res) => {
+    const postId = req.params.id;
+    const { userId } = req.body;
+
+    try {
+        const post = await PostModel.findById(postId);
+        if (post.userId === userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("Post Updated");
+        } else {
+            res.status(403).json("Action forbidden");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+// Delete a post
+export const deletePost = async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
+
+    try {
+        const post = await PostModel.findById(id);
+        if (post.userId === userId) {
+            await post.deleteOne();
+            res.status(200).json("POst deleted successfully");
+        } else {
+            res.status(403).json("Action forbidden");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
