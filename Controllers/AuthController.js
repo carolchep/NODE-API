3d56bc33b708e746646b266a0f3fa9da0abe7a08
carolchep <carolchepsam@gmail.com>
2022-06-14 10:19:37 +0300

    jwtwebtoken

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,57 +1,61 @@
-import UserModel from "../Models/userModel.js";
+import UserModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-// Registering a new User
+// Register new user
 export const registerUser = async (req, res) => {
-    //destrucure the request body
-    const { username, password, firstname, lastname } = req.body;
-//use bcrypy to hash passsword
-    const salt = await bcrypt.genSalt(10);
-    //attached hashed value to the passsword
-    const hashedPass = await bcrypt.hash(password, salt);
-//create an object instance with the values
-    const newUser = new UserModel({
-        username,
-        password: hashedPass,
-        firstname,
-        lastname,
-    });
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPass
+    const newUser = new UserModel(req.body);
+    const {username} = req.body
     try {
-        //save the object to the database
-        await newUser.save();
-        res.status(200).json(newUser);
+        // addition new
+        const oldUser = await UserModel.findOne({ username });
+
+        if (oldUser)
+            return res.status(400).json({ message: "User already exists" });
+
+        // changed
+        const user = await newUser.save();
+        const token = jwt.sign(
+            { username: user.username, id: user._id },
+            process.env.JWTKEY,
+            { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
     } catch (error) {
-        //error display
         res.status(500).json({ message: error.message });
     }
 };
 
+// Login User
 
-// login User
-
+// Changed
 export const loginUser = async (req, res) => {
-    //destructure the values
-    const {username, password} = req.body
+    const { username, password } = req.body;
 
     try {
-        //find the username in the db
-        const user = await UserModel.findOne({username: username})
+        const user = await UserModel.findOne({ username: username });
 
+        if (user) {
+            const validity = await bcrypt.compare(password, user.password);
 
-        if(user)
-        //if user exists
-        {
-            //use compare to check if paasword is the same
-            const validity = await bcrypt.compare(password, user.password)
-
-
-            validity? res.status(200).json(user): res.status(400).json("Invalid password")
+            if (!validity) {
+                res.status(400).json("wrong password");
+            } else {
+                const token = jwt.sign(
+                    { username: user.username, id: user._id },
+                    process.env.JWTKEY,
+                    { expiresIn: "1h" }
+                );
+                res.status(200).json({ user, token });
+            }
+        } else {
+            res.status(404).json("User not found");
         }
-        else{
-            res.status(404).json("User does not exists")
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    } catch (err) {
+        res.status(500).json(err);
     }
-}
+};
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoutes.js'
 import PostRoute from './Routes/PostRoute.js'
-
+import cors from 'cors'
 // Routes
 
 const app = express();
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
+app.use(cors())
 dotenv.config();
 
 mongoose
